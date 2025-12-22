52cce37bac046962a51a59aeebad97c3a79fe98c
Prathamesh Salunke <salunkep341@gmail.com>
2025-12-22 13:03:09 +0530

    added healthcheck

---
+++ src/main/java/com/skill/taskassigner/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.skill.taskassigner.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Skill Task Backend is running";
+    }
+}
